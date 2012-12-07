4f235d08462d8bb54594688c4dd276b8b1fcd4b4
pyciak <pyciak1@gmail.com>
2012-12-07 10:55:25 +0100

    update

---
+++ src/pkginterface/listEdit.java
@@ -0,0 +1,59 @@
+package pkginterface;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+/**
+ *
+ * @author Pyciak
+ */
+public class listEdit extends JFrame
+{
+    JLabel label;
+    JButton button;
+    JTextField field;
+    JFrame frame;
+    
+    listEdit(int id)
+    {
+        setTitle("Edytuj Listę");
+        setSize(250, 135);
+        setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        this.setResizable(false);
+        setLayout(null);
+        
+        frame = this;
+        
+        label = new JLabel("Nazwa:");
+        button = new JButton("Zapisz");
+        
+        field = new JTextField(getCategoryName(id));
+        
+        label.setLocation(20,20);
+        label.setSize(50,25);
+        field.setLocation(60,20);
+        field.setSize(160,25);
+        button.setLocation(150,60);
+        button.setSize(70,25);
+        
+        button.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                //Zapisanie
+                
+                frame.setVisible(false);
+            }
+        });
+        
+        add(label);
+        add(field);
+        add(button); 
+    }
+    private String getCategoryName(int id)
+    {
+        //pobranie z bazy danych nazwy listy z id
+        
+        return "nazwa"+id; //tymczasowe
+    }
+}
---
+++ src/pkginterface/settingBase.java
@@ -0,0 +1,61 @@
+package pkginterface;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+/**
+ *
+ * @author Pyciak
+ */
+public class settingBase extends JFrame
+{
+    JLabel label1,label2;
+    JButton button;
+    JFrame frame;
+    JCheckBox box1,box2;
+    
+    settingBase()
+    {
+        setTitle("Zródło danych:");
+        setSize(220, 155);
+        setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        this.setResizable(false);
+        setLayout(null);
+        
+        frame = this;
+        
+        label1 = new JLabel("Baza lokalna:");
+        label2 = new JLabel("Baza na serwerze:");
+        box1 = new JCheckBox();
+        box2 = new JCheckBox();
+        button = new JButton("Zapisz");
+        
+        label1.setLocation(35,20);
+        label1.setSize(100,25);
+        box1.setLocation(155,20);
+        box1.setSize(25,25);
+        label2.setLocation(35,50);
+        label2.setSize(100,25);
+        box2.setLocation(155,50);
+        box2.setSize(25,25);
+        button.setLocation(105,80);
+        button.setSize(70,25);
+        
+        button.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                //Zapisanie
+                
+                frame.setVisible(false);
+            }
+        });
+        
+        
+        add(label1);
+        add(box1);
+        add(label2);
+        add(box2);
+        add(button);  
+    }
+}
